335e0431d489a743d8480432bf3e7dc18c3b61ee
Mdrahil1911 <mdrahil1911@gmail.com>
2024-11-12 20:26:53 +0530

    Initial commit with project files

---
+++ tests/test_login.py
@@ -0,0 +1,22 @@
+import time
+import pytest
+from pages.login_page import LoginPage
+from utils.config import driver
+
+@pytest.mark.login
+def test_valid_login(driver):
+    """Verify successfull login."""
+    login_page =LoginPage(driver)
+
+    driver.get("https://app.nfttrace.com/login")
+    driver.maximize_window()
+    login_page.enter_email("superadmin@chaincodeconsulting.com")
+    login_page.enter_password("admin@123")
+    login_page.click_login()
+    time.sleep(1)
+
+    expected_title = "NFTtrace - Projects"
+    assert driver.title == expected_title, f"Expected title '{expected_title}', but got '{driver.title}'"
+
+
+
---
+++ utils/config.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from pages.login_page import LoginPage
+
+
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome()
+
+    yield driver
+    driver.quit()
+
+# def driver():
+#     chrome_options = Options()
+#     chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+#     chrome_options.add_argument("--no-sandbox")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
+#
+#     driver = webdriver.Chrome(options=chrome_options)
+#     return driver
+
+
+BASE_URL = "https://app.nfttrace.com/login"
+USERNMAE = "superadmin@gmail.com"
+PASSWORD = "admin@123"
+
+@pytest.fixture(scope="function")
+def login(driver):
+    driver.maximize_window()
+    driver.get(BASE_URL)
+    login_page = LoginPage(driver)
+    login_page.enter_email(USERNMAE)
+    login_page.enter_password(PASSWORD)
+    login_page.click_login()
+    yield
+
